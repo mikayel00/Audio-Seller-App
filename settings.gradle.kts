53c725721c86710e8ba587d4c0f513bc7aac23d4
Mikayel <mikayel.hovhannisyan27@gmail.com>
2023-02-24 15:06:10 +0400

    starting application

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "Audio-Seller-App"
---
+++ src/test/kotlin/com/example/audiosellerapp/AudioSellerAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.audiosellerapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AudioSellerAppApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
